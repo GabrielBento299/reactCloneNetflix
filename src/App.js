22d85c0c81da186b3f6d7a9b4e1f5ec64c4c883f
GabrielBento299 <gabrieljesus100-2010@hotmail.com>
2022-02-01 10:20:03 -0300

    three commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React, { useEffect, useState} from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow';
+import MovieRow from './components/MoiveRow/MovieRow'; 
+import FeaturedMovie from './components/FeaturedMovies.js/FeaturedMovie';
  
 // eslint-disable-next-line import/no-anonymous-default-export
 export default  () => {
 
   const [movieList, setMovieList] = useState ([]);
-
-
+  const [featuredData, setFeaturedData] = useState ([]);
+  
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      setMovieList(list)
-     }
+      setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+    }
 
     loadAll()
   }, []);
 
   return(
     <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+    
+
       <section className="lists">
         {movieList.map((item, key)=> (
           <MovieRow 
-           key={key}
+            key={key}
             title={item.title}
             items={item.items}
           />
